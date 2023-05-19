dd6110478829ceee9f40d5cdd81960ae664671ac
brianMunyao <brianmunyao6@gmail.com>
2023-05-19 11:18:02 +0300

    added page templates

---
+++ wp-content/themes/AppTheme/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name') ?></title>
+</head>
+
+<body>
+    <nav><?php bloginfo('name') ?></nav>
+
+    <style>
+        *,
+        html {
+            padding: 0;
+            margin: 0;
+        }
+
+        nav {
+            padding: 20px 100px;
+            background: teal;
+            color: #fff;
+
+        }
+
+        .app-container {
+            padding: 20px;
+        }
+
+        footer {
+            padding: 20px;
+            opacity: .4;
+        }
+    </style>
+
+    <div class="app-container">
