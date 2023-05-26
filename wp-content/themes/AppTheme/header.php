3d15b0965b69dd26032d87c634a34ae2d78afdb3
brianMunyao <brianmunyao6@gmail.com>
2023-05-26 11:14:35 +0300

    assessment 8

--- wp-content/themes/AppTheme/header.php
+++ wp-content/themes/AppTheme/header.php
@@ -6,33 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name') ?></title>
+    <?php wp_head(); ?>
 </head>
 
 <body>
     <nav><?php bloginfo('name') ?></nav>
-
-    <style>
-        *,
-        html {
-            padding: 0;
-            margin: 0;
-        }
-
-        nav {
-            padding: 20px 100px;
-            background: teal;
-            color: #fff;
-
-        }
-
-        .app-container {
-            padding: 30px 100px;
-        }
-
-        footer {
-            padding: 20px 100px;
-            opacity: .4;
-        }
-    </style>
 
     <div class="app-container">
